67008feb0156780d6394249036b4b35837a0cecf
Heitor Fonseca <heitorffonseca@gmail.com>
2022-02-04 00:48:46 -0300

    initial project and some pages

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from '../src/components/Link'
+
+export default function Page404() {
+    return (
+        <div>
+            <h1>Alura Cases - Error 404</h1>
+            <Link href="/">
+                Voltar para a home
+            </Link>
+        </div>
+    )
+}
